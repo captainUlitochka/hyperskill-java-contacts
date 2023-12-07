dd54de8d64e5240ce3c6e5c47862c3cc0a64f79d
Olga Fattakhova <olga.fattakhova@pprcard.ru>
2023-12-07 19:03:31 +0300

    2nd stage WIP: bad thing for input reading for now

--- Contacts (Java)/task/src/contacts/Utils.java
+++ Contacts (Java)/task/src/contacts/Utils.java
@@ -1,11 +1,15 @@
 package contacts;
 
+import java.io.*;
 import java.util.Scanner;
 
 public class Utils {
     Scanner reader;
 
     Utils() {
+        reader = new Scanner(System.in);
+    }
+    public void read() {
         reader = new Scanner(System.in);
     }
 }
