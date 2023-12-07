ddc9fccb86ba6f0ea445badb0980c5650aa5bf8b
Olga Fattakhova <olga.fattakhova@pprcard.ru>
2023-12-07 19:04:11 +0300

    2nd stage WIP: getters, setters and checks for Contact class

--- Contacts (Java)/task/src/contacts/Contact.java
+++ Contacts (Java)/task/src/contacts/Contact.java
@@ -1,9 +1,36 @@
 package contacts;
 
 public class Contact {
+    private int id;
     private String firstName;
     private String lastName;
     private String phoneNumber;
+    public Contact(int id, String inputFirstName, String inputLastName, String inputNumber) {
+        setId(id);
+        setFirstName(inputFirstName);
+        setLastName(inputLastName);
+        setPhoneNumber(inputNumber);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
 
     public void setFirstName(String firstName) {
         this.firstName = firstName;
@@ -14,12 +41,13 @@ public class Contact {
     }
 
     public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
+        if (checkPhoneNumber(phoneNumber)) this.phoneNumber = phoneNumber;
+        else this.phoneNumber = Messages.NO_NUMBER.getMessage();
     }
 
-    public Contact(String inputFirstName, String inputLastName, String inputNumber) {
-        setFirstName(inputFirstName);
-        setLastName(inputLastName);
-        setPhoneNumber(inputNumber);
+    private boolean checkPhoneNumber(String phoneNumber) {
+        String regex = "\\+?(\\(\\w+\\)|\\w+[ -]\\(\\w{2,}\\)|\\w+)([ -]\\w{2,})*";
+        return phoneNumber.matches(regex);
     }
+
 }
