9bd740c6bc2b8ae3e84864ebba4d1297e4afa804
Olga Fattakhova <olga.fattakhova@pprcard.ru>
2023-12-07 19:03:03 +0300

    2nd stage WIP: new strings

--- Contacts (Java)/task/src/contacts/Messages.java
+++ Contacts (Java)/task/src/contacts/Messages.java
@@ -1,12 +1,20 @@
 package contacts;
 
 public enum Messages {
+    COMMAND_LIST("Enter action (add, remove, edit, count, list, exit): "),
+    RECORDS_COUNT("The Phone Book has %d records.%n"),
     ENTER_NAME("Enter the name of the person:"),
     ENTER_SURNAME("Enter the surname of the person:"),
     ENTER_NUMBER("Enter the number:"),
-    RECORD_SUCCESS("A record created!"),
-    NUM_OF_NEW_RECORDS("A Phone Book with a single record created!"),
-    INVALID_DATA("Invalid %s");
+    RECORD_SUCCESS("The record %s."),
+    NOTHING_TO_DO("No records to %s"),
+    INVALID_DATA("Invalid %s"),
+    INVALID_CMD("Invalid command!"),
+    NO_NUMBER("[no number]"),
+    WRONG_FORMAT("Wrong number format!"),
+    SELECT_RECORD("Select a record: "),
+    SELECT_FIELD("Select a field (name, surname, number): "),
+    RECORD("%d. %s %s, %s%n");
 
     private final String message;
 
